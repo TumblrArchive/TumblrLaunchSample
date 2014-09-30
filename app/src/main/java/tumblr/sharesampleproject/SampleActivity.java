c3ee4dd8b514b77c1ff0f0b2a69adefe00b62686
Brandon Denney <brandon@tumblr.com>
2014-09-30 19:21:43 -0400

    Created a 'gistable' TumblrShareUtils.

---
+++ app/src/main/java/tumblr/sharesampleproject/SampleActivity.java
@@ -0,0 +1,162 @@
+package tumblr.sharesampleproject;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Sample activity. This could be YOUR app!
+ *
+ * @author haseman
+ */
+public class SampleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new SampleFragment())
+					.commit();
+		}
+	}
+
+	/**
+	 * A sample fragment containing a few text views.
+	 *
+	 * @author haseman
+	 */
+	public static class SampleFragment extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		                         Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+			TextView tv = (TextView) rootView.findViewById(R.id.create_text_post);
+			tv.setOnClickListener(mTextPostListener);
+
+			tv = (TextView) rootView.findViewById(R.id.create_link_post);
+			tv.setOnClickListener(mLinkPostListner);
+
+			tv = (TextView) rootView.findViewById(R.id.create_photo_post);
+			tv.setOnClickListener(mPhotoPostListener);
+
+			tv = (TextView) rootView.findViewById(R.id.create_multiple_photo_post);
+			tv.setOnClickListener(mMultiPhotoPostListener);
+
+			tv = (TextView) rootView.findViewById(R.id.view_blog);
+			tv.setOnClickListener(mViewBlogListener);
+
+			tv = (TextView) rootView.findViewById(R.id.view_post);
+			tv.setOnClickListener(mViewPostListener);
+
+			tv = (TextView) rootView.findViewById(R.id.view_tag);
+			tv.setOnClickListener(mViewTagListener);
+
+			// Check for the Tumblr app
+			if (!TumblrShareUtils.isTumblrInstalled(getActivity())) {
+				Toast.makeText(getActivity().getApplicationContext(), "Tumblr is not on your phone, " +
+						"you're gonna have a bad time", Toast.LENGTH_LONG).show();
+			}
+
+			return rootView;
+		}
+
+		private final View.OnClickListener mTextPostListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Create the text post intent.
+				final Intent textPostIntent = TumblrShareUtils.ShareIntentCreator.createShareTextIntent(
+						"This is the post title.", "This is the body text.");
+
+				// Share!
+				getActivity().startActivity(textPostIntent);
+			}
+		};
+
+		private final View.OnClickListener mLinkPostListner = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Create the link post intent.
+				final Intent linkPostIntent = TumblrShareUtils.ShareIntentCreator.createShareLinkIntent(
+						"Title of the link post", Uri.parse("http://tumblr.com"));
+
+				// Share!
+				getActivity().startActivity(linkPostIntent);
+			}
+		};
+
+		private final View.OnClickListener mPhotoPostListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Create the photo post intent.
+				final Intent photoPostIntent = TumblrShareUtils.ShareIntentCreator.createSharePhotoIntent(
+						Uri.parse("content://media/external/images/media/1"));
+
+				// Share!
+				getActivity().startActivity(photoPostIntent);
+			}
+		};
+
+		private final View.OnClickListener mMultiPhotoPostListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Create a photo post with several photos (using sample Uri's).
+				final List<Uri> photoList = new ArrayList<Uri>();
+				photoList.add(Uri.parse("content://media/external/images/media/1"));
+				photoList.add(Uri.parse("content://media/external/images/media/2"));
+				photoList.add(Uri.parse("content://media/external/images/media/3"));
+
+				// Create the photoset post intent.
+				final Intent photosetPostIntent = TumblrShareUtils.ShareIntentCreator.createSharePhotosetIntent(photoList);
+
+				// Share!
+				getActivity().startActivity(photosetPostIntent);
+			}
+		};
+
+		private final View.OnClickListener mViewBlogListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Deep link to a blog.
+				final Uri blogDataUri = TumblrShareUtils.DeepLinkCreator.createBlogDeepLink("haseman");
+
+				// Start the Tumblr app.
+				TumblrShareUtils.startTumblr(getActivity(), blogDataUri);
+			}
+		};
+
+		private final View.OnClickListener mViewPostListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Deep link to a specific post.
+				final Uri postDataUri = TumblrShareUtils.DeepLinkCreator.createBlogDeepLink("haseman", 94288096341L);
+
+				// Start the Tumblr app.
+				TumblrShareUtils.startTumblr(getActivity(), postDataUri);
+			}
+		};
+
+		private final View.OnClickListener mViewTagListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Deep link to a search results page.
+				final Uri searchDataUri = TumblrShareUtils.DeepLinkCreator.createSearchDeepLink("cars");
+
+				// Start the Tumblr app.
+				TumblrShareUtils.startTumblr(getActivity(), searchDataUri);
+			}
+		};
+	}
+}
---
+++ app/src/main/java/tumblr/sharesampleproject/TumblrShareUtils.java
@@ -0,0 +1,314 @@
+package tumblr.sharesampleproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tumblr sharing superclass that allows for deep linking into the Tumblr app and sharing content to create new
+ * Tumblr posts.
+ *
+ * @author brandon
+ */
+public class TumblrShareUtils {
+	/**
+	 * The Tumblr package name.
+	 */
+	public static final String TUMBLR_PACKAGE_NAME = "com.tumblr";
+
+	/**
+	 * Starts the Tumblr application.
+	 *
+	 * @param activity
+	 * 		the activity to use to start the Tumblr app.
+	 * @param data
+	 * 		the data Uri to open from within Tumblr. This should be a Uri created from the {@link DeepLinkCreator}.
+	 */
+	public static void startTumblr(final Activity activity, final Uri data) {
+		// Defensive.
+		if (activity == null || data == null) {
+			return;
+		}
+
+		final Intent viewIntent = new Intent();
+		viewIntent.setAction(Intent.ACTION_VIEW);
+		viewIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+		viewIntent.setPackage(TumblrShareUtils.TUMBLR_PACKAGE_NAME);
+		viewIntent.setData(data);
+		activity.startActivity(viewIntent);
+	}
+
+	/**
+	 * Tests whether the Tumblr app is installed.
+	 *
+	 * @param context
+	 * 		the android context to use.
+	 * @return true if the Tumblr app is installed, false otherwise.
+	 */
+	public static boolean isTumblrInstalled(final Context context) {
+		// Defensive.
+		if (context == null) {
+			return false;
+		}
+
+		final PackageManager packageManager = context.getPackageManager();
+		final List<ApplicationInfo> apps = packageManager.getInstalledApplications(0);
+		boolean found = false;
+		for (ApplicationInfo app : apps) {
+			if (TumblrShareUtils.TUMBLR_PACKAGE_NAME.equals(app.packageName)) {
+				found = true;
+				break;
+			}
+		}
+		return found;
+	}
+
+	/**
+	 * Creates a deep link in the Tumblr application.
+	 *
+	 * @author brandon
+	 */
+	public static class DeepLinkCreator {
+		/**
+		 * Base format for all Tumblr URLs.
+		 */
+		private static final String URL_FORMAT_BASE = "tumblr://x-callback-url/";
+
+		/**
+		 * Creates a deep link URL to a blog inside the Tumblr Android app.
+		 *
+		 * @param blogName
+		 * 		the name of the blog to open inside of the Tumblr app.
+		 * @return the deep link URL.
+		 */
+		public static Uri createBlogDeepLink(final String blogName) {
+			return createBlogDeepLink(blogName, 0);
+		}
+
+		/**
+		 * Creates a deep link URL to a blog inside the Tumblr Android app, to a specific post.
+		 *
+		 * @param blogName
+		 * 		the name of the blog to open inside of the Tumblr app.
+		 * @param postId
+		 * 		the post to open the blog to. This post will be at the top of the user's stream.
+		 * @return the deep link URL.
+		 */
+		public static Uri createBlogDeepLink(final String blogName, final long postId) {
+			String link = URL_FORMAT_BASE + "blog/";
+
+			final String sanitizedBlogName = sanitizeBlogName(blogName);
+
+			if (!TextUtils.isEmpty(sanitizedBlogName)) {
+				link += "?blogName=" + sanitizedBlogName;
+			}
+
+			// Check the post ID.
+			if (postId > 0) {
+				link += "&postID=" + String.valueOf(postId);
+			}
+
+			return Uri.parse(link);
+		}
+
+		/**
+		 * Creates a deep link URL to a search result screen inside the Tumblr Android app.
+		 *
+		 * @param searchTerm
+		 * 		the term to search for.
+		 * @return the deep link URL.
+		 */
+		public static Uri createSearchDeepLink(final String searchTerm) {
+			String link = URL_FORMAT_BASE + "tag/";
+
+			if (!TextUtils.isEmpty(searchTerm)) {
+				link += "?tag=" + searchTerm;
+			}
+
+			return Uri.parse(link);
+		}
+
+		/**
+		 * Sanitizes a blog name.
+		 *
+		 * @param blogName
+		 * 		the name to sanitize.
+		 * @return the sanitized blog name.
+		 */
+		private static String sanitizeBlogName(final String blogName) {
+			// Defensive.
+			if (TextUtils.isEmpty(blogName)) {
+				return "";
+			}
+
+			String sanitizedBlogName = blogName;
+
+			// If the blog name contains ".tumblr.com", then remove it. The Android app will not handle this case and
+			// only expects the blog name.
+			if (sanitizedBlogName.contains(".tumblr.com")) {
+				sanitizedBlogName = sanitizedBlogName.replace(".tumblr.com", "");
+			}
+
+			return sanitizedBlogName;
+		}
+	}
+
+	/**
+	 * Builds intents that will share content into the Tumblr app.
+	 *
+	 * @author brandon
+	 */
+	public static class ShareIntentCreator {
+		/**
+		 * The package name for the Tumblr app.
+		 */
+		private static final String TUMBLR_PACKAGE_NAME = "com.tumblr";
+
+		/**
+		 * Creates an intent that will open and populate the text post form in the Tumblr app.
+		 *
+		 * @param title
+		 * 		the title of the text post.
+		 * @param body
+		 * 		the body of the text post.
+		 * @return the share intent.
+		 */
+		public static Intent createShareTextIntent(final String title, final String body) {
+			// Create the base intent.
+			final Intent textPostIntent = createBaseShareIntent();
+
+			// Set the content type.
+			textPostIntent.setType("text/plain");
+
+			// Add the title if one was given.
+			if (!TextUtils.isEmpty(title)) {
+				textPostIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+			}
+
+			// Add the body if one was given.
+			if (!TextUtils.isEmpty(body)) {
+				textPostIntent.putExtra(Intent.EXTRA_TEXT, "Body text is here");
+			}
+
+			return textPostIntent;
+		}
+
+		/**
+		 * Creates an intent that will open and populate the link post form in the Tumblr app.
+		 *
+		 * @param url
+		 * 		the URL of the link post.
+		 * @return the share intent.
+		 */
+		public static Intent createShareLinkIntent(final Uri url) {
+			return createShareLinkIntent("", url);
+		}
+
+		/**
+		 * Creates an intent that will open and populate the link post form in the Tumblr app.
+		 *
+		 * @param title
+		 * 		the title of the link post.
+		 * @param url
+		 * 		the URL of the link post.
+		 * @return the share intent.
+		 */
+		public static Intent createShareLinkIntent(final String title, final Uri url) {
+			// Create the base intent.
+			final Intent linkPostIntent = createBaseShareIntent();
+
+			// Set the content type.
+			linkPostIntent.setType("text/plain");
+
+			// Set the title if we have one.
+			if (!TextUtils.isEmpty(title)) {
+				linkPostIntent.putExtra(Intent.EXTRA_SUBJECT, "Title of the link post");
+			}
+
+			// Set the URL if we have one.
+			if (url != null) {
+				linkPostIntent.putExtra(Intent.EXTRA_TEXT, "http://tumblr.com");
+			}
+
+			return linkPostIntent;
+		}
+
+		/**
+		 * Creates an intent that will open and populate the photo post form in the Tumblr app.
+		 *
+		 * @param imageUri
+		 * 		the URI of the image to share.
+		 * @return the share intent.
+		 */
+		public static Intent createSharePhotoIntent(final Uri imageUri) {
+			// Take the image URI, and make it into a list.
+			final List<Uri> imageUris = new ArrayList<Uri>();
+			if (imageUri != null) {
+				imageUris.add(imageUri);
+			}
+
+			// Create a photoset of one.
+			return createSharePhotosetIntent(imageUris);
+		}
+
+		/**
+		 * Creates an intent that will open and populate the photo post form in the Tumblr app.
+		 *
+		 * @param imageUris
+		 * 		the image URIs to share.
+		 * @return the share intent.
+		 */
+		public static Intent createSharePhotosetIntent(final List<Uri> imageUris) {
+			// Create the base intent.
+			final Intent photoPostIntent = createBaseShareIntent();
+
+			// Override the action.
+			photoPostIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+
+			// Set the content type.
+			photoPostIntent.setType("image/*");
+
+			// Add the image URI if we have it.
+			if (imageUris != null && !imageUris.isEmpty()) {
+				photoPostIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, sanitizePhotoList(imageUris));
+			}
+
+			return photoPostIntent;
+		}
+
+		/**
+		 * Sanitizes the photo list for consumption by the Tumblr app.
+		 *
+		 * @return the sanitized photo list.
+		 */
+		private static ArrayList<Uri> sanitizePhotoList(final List<Uri> imageUris) {
+			// Create the return list.
+			final ArrayList<Uri> sanitizedImageUris = new ArrayList<Uri>();
+
+			// Ensure that the list is populated.
+			if (imageUris != null && !imageUris.isEmpty()) {
+				// Copy everything over.
+				sanitizedImageUris.addAll(imageUris);
+			}
+
+			return sanitizedImageUris;
+		}
+
+		/**
+		 * @return a new base share intent that sets the correct action and package.
+		 */
+		private static Intent createBaseShareIntent() {
+			final Intent shareIntent = new Intent();
+			shareIntent.setAction(Intent.ACTION_SEND);
+			shareIntent.setPackage(TUMBLR_PACKAGE_NAME);
+			return shareIntent;
+		}
+	}
+}
