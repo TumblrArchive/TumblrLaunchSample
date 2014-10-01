52aee814dc29479f05158b9c7fa21c388b07491f
Chris Haseman <haseman@tumblr.com>
2014-10-01 13:43:13 -0400

    adding license notices to correct files

--- app/src/androidTest/java/tumblr/sharesampleproject/ApplicationTest.java
+++ app/src/androidTest/java/tumblr/sharesampleproject/ApplicationTest.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
+ * Copyright (c) 2014 Tumblr. All rights reserved.
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
